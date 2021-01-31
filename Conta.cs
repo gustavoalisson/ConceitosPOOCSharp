55d54eca008d69f47b357977bbc55959e20bab57
Alisson Gustavo <gustavoalisson112@gmail.com>
2021-01-31 03:08:15 -0300

    criação de classe, objeto e metodos

---
+++ Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundamentosPOO
+{
+    public class Conta
+    {
+        private double Saldo { get; set; }
+        public double Limite { get; private set; }
+        public int Numero { get; set; }
+
+        //Metodos
+        public void Deposita(double valor)
+        {
+            this.Saldo = this.Saldo + valor;
+        }
+
+        public bool Saca(double valor)
+        {
+            double saldoDisponivel = this.ConsultaSaldoDisponivel();
+            
+            if (valor > saldoDisponivel)
+            {              
+                Console.WriteLine("Saque indisponível"); 
+                return false;
+            }
+            else
+            {
+                this.Saldo = this.Saldo - valor;
+                return true;
+            }   
+        }
+
+        public void AdicionarLimite(double valor)
+        {
+            this.Limite = valor;
+        }
+
+        public double ConsultaSaldoDisponivel()
+        {
+            return this.Saldo + this.Limite;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FundamentosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldoTotal = 0;
+            bool validaSaque;
+
+            Console.WriteLine("-------Conta bancária-------");
+
+            // criação do objeto = instância da classe Conta
+            Conta conta1 = new Conta();
+
+            conta1.AdicionarLimite(1000);
+
+            conta1.Deposita(2000);
+            conta1.Deposita(5000);
+
+            validaSaque = conta1.Saca(50000);
+
+            if (validaSaque)
+            {
+                saldoTotal = conta1.ConsultaSaldoDisponivel();
+
+                Console.WriteLine("Saldo disponível: " + saldoTotal);
+                Console.WriteLine("Limite disponível: " + conta1.Limite);
+            }
+          
+        }
+    }
+}
