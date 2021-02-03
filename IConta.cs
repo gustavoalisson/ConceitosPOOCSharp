919def50576c02a19b28cbe12fed5b02d255e7f7
Alisson Gustavo <gustavoalisson112@gmail.com>
2021-02-03 00:21:08 -0300

    implementação da interface conta

---
+++ IConta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundamentosPOO
+{
+    // realiza o contrato de assinatura dos metodos
+    public interface IConta
+    {
+        void Deposita(double valor);
+        bool Saca(double valor);
+        void AdicionarLimite(double valor);
+        double ConsultaSaldoDisponivel();
+    }
+}
